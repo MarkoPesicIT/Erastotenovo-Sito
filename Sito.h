9f69ee88c4e974b9ef68ba358293ab98fe8ef1bd
MarkoPesic1605 <112968936+MarkoPesicIT@users.noreply.github.com>
2023-03-01 20:48:13 +0100

    Update Sito.h

--- Sito.h
+++ Sito.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -8,15 +8,15 @@ class Sito
 {
 public:
 
-    vector<bool> eratostenovoSito(int n)
+    vector<bool> eratostenovoSito(long long int n)
     {
         vector<bool> prost(n + 1, true);
         prost[0] = prost[1] = false;
-        for (int i = 2; i * i <= n; i++)
+        for (long long int i = 2; i * i <= n; i++)
         {
             if (prost[i])
             {
-                for (int j = i * i; j <= n; j += i)
+                for (long long int j = i * i; j <= n; j += i)
                 {
                     prost[j] = false;
                 }
@@ -24,5 +24,4 @@ public:
         }
         return prost;
     }
-
 };
