46446de8e3bbbc085311239ef929594ab72944eb
MarkoPesic1605 <112968936+MarkoPesicIT@users.noreply.github.com>
2023-03-01 20:30:40 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <fstream>
 #include "Sito.h"
 #include "NZD.h"
 
@@ -8,7 +9,8 @@ using namespace std;
 
 int main()
 {
-    int opcija, n;
+    int opcija;
+    long long n;
     cout << "Izaberite opciju:" << endl;
     cout << "Da li je uneti broj prost      |1|" << endl;
     cout << "Svi prosti brojevi do n        |2|" << endl;
@@ -44,17 +46,79 @@ int main()
 
         cout << "Unesite broj N: ";
         cin >> n;
-        Sito v2;
-        vector<bool> prost = v2.eratostenovoSito(n);
-        for (int i = 2; i <= n; i++)
+        cout << "Ispis preko:\n";
+        cout << "Konzole                            |1|\n";
+        cout << ".txt fail (dosta brzi od koznole)  |2|\n";
+        if (n<10*10^6)
         {
-            if (prost[i])
+            int opcija2;
+            cin >> opcija2;
+            Sito v2;
+            vector<bool> prost = v2.eratostenovoSito(n);
+            switch (opcija2)
             {
-                cout << i << " ";
+            case 1:
+                for (int i = 2; i <= n; i++)
+                {
+                    if (prost[i])
+                    {
+                        cout << i;
+                    }
+                }
+                break;
+
+            case 2:
+
+                ofstream outputFile("izlaz.txt");
+                for (int i = 2; i <= n; i++)
+                {
+                    if (prost[i])
+                    {
+                        outputFile << i << endl;
+                    }
+                }
+                outputFile.close();
+                break;
             }
+            break;
         }
-        cout << endl;
-        break;
+        else
+        {
+            cout << "Brojevi veci od 10 miliona mogu zablokirati kompjuter\n";
+            cout << "Da li zelis da nastavis?\n";
+            cout << "DA |1|\n";
+            cout << "NE |2|\n";
+            int opcija2;
+            cin >> opcija2;
+            Sito v2;
+            vector<bool> prost = v2.eratostenovoSito(n);
+            switch (opcija2)
+            {
+            case 1:
+                for (int i = 2; i <= n; i++)
+                {
+                    if (prost[i])
+                    {
+                        cout << i;
+                    }
+                }
+                break;
+
+            case 2:
+
+                ofstream outputFile("izlaz.txt");
+                for (int i = 2; i <= n; i++)
+                {
+                    if (prost[i])
+                    {
+                        outputFile << i << endl;
+                    }
+                }
+                outputFile.close();
+                break;
+            }
+            break;
+        }
     }
     return 0;
 }
